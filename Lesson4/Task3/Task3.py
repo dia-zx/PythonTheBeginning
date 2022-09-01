13c8b7047a8bc7b9007f8ea820b1a0e2c07fa58c
dia_zx <dia_zx@mail.ru>
2022-09-01 17:07:53 +0300

    задачи 1...4

---
+++ Lesson4/Task3/Task3.py
@@ -0,0 +1,33 @@
+﻿#Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена 
+# и записать в файл многочлен степени k. 
+# 
+# Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+
+
+def solution(k):
+    st = ""
+    for i in range(k, -1, -1):
+        r = randint(0, 100)
+        if r == 0:
+            continue
+        if len(st):
+            st += " + "
+        if i == 0:
+            st += str(r)
+            continue
+        if i == 1:
+            st += str(r) + "*X"
+            continue
+        st += str(r) + "*X^" + str(i)
+    st += " = 0"
+    return st
+
+
+f = open("file.txt","w")
+f.write(solution(5))
+f.close()
+
+print(solution(5))
